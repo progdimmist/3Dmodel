0587e01f07b4c6c86749fa0a93c8f26eef0623ab
Kostya Zinchenko <zinchenkokostantin@yandex.ru>
2023-01-06 21:15:16 +0300

    заменены вектора в ObjReaderTest

--- tests/com/cgvsu/objreader/ObjReaderTest.java
+++ tests/com/cgvsu/objreader/ObjReaderTest.java
@@ -1,5 +1,6 @@
 package com.cgvsu.objreader;
 
+import com.cgvsu.math.vector.Vector3F;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,16 +12,16 @@ class ObjReaderTest {
     @Test
     public void testParseVertex01() {
         ArrayList<String> wordsInLineWithoutToken = new ArrayList<>(Arrays.asList("1.01", "1.02", "1.03"));
-        Vector3f result = ObjReader.parseVertex(wordsInLineWithoutToken, 5);
-        Vector3f expectedResult = new Vector3f(1.01f, 1.02f, 1.03f);
+        Vector3F result = ObjReader.parseVertex(wordsInLineWithoutToken, 5);
+        Vector3F expectedResult = new Vector3F(1.01f, 1.02f, 1.03f);
         Assertions.assertTrue(result.equals(expectedResult));
     }
 
     @Test
     public void testParseVertex02() {
         ArrayList<String> wordsInLineWithoutToken = new ArrayList<>(Arrays.asList("1.01", "1.02", "1.03"));
-        Vector3f result = ObjReader.parseVertex(wordsInLineWithoutToken, 5);
-        Vector3f expectedResult = new Vector3f(1.01f, 1.02f, 1.10f);
+        Vector3F result = ObjReader.parseVertex(wordsInLineWithoutToken, 5);
+        Vector3F expectedResult = new Vector3F(1.01f, 1.02f, 1.10f);
         Assertions.assertFalse(result.equals(expectedResult));
     }
 
